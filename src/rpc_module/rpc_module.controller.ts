21c38506c6fb52dbd8fb93069db17a1a3f1b75a9
MrSharipov <84614549+MrSharipov@users.noreply.github.com>
2023-01-10 12:58:30 +0500

    upd

--- src/rpc_module/rpc_module.controller.ts
+++ src/rpc_module/rpc_module.controller.ts
@@ -1,4 +1,27 @@
 import { Controller } from '@nestjs/common';
+import {
+  RpcId,
+  RpcPayload,
+  RpcVersion,
+  RpcMethod,
+  IRpcHandler,
+  RpcHandler,
+} from '@jashkasoft/nestjs-json-rpc';
 
+@RpcHandler({
+  method: 'test',
+})
+class Payload {
+  city: string;
+}
 @Controller('rpc-module')
-export class RpcModuleController {}
+export class RpcModuleController implements IRpcHandler<Payload> {
+  public async invoke(
+    @RpcPayload() payload: Payload,
+    @RpcVersion() version: string,
+    @RpcId() id: number | string,
+    @RpcMethod() method: string,
+  ) {
+    return payload;
+  }
+}
