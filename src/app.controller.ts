b83037d7bf11ffa6e82821ff9093dfce224adb70
MrSharipov <84614549+MrSharipov@users.noreply.github.com>
2023-01-10 12:53:01 +0500

    upd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,38 +1,12 @@
-// import { Controller, Get } from '@nestjs/common';
-// import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
-// @Controller()
-// export class AppController {
-//   constructor(private readonly appService: AppService) {}
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
 
-//   @Get()
-//   getHello(): string {
-//     return this.appService.getHello();
-//   }
-// }
-
-import {
-  RpcId,
-  RpcPayload,
-  RpcVersion,
-  RpcMethod,
-  IRpcHandler,
-  RpcHandler,
-} from '@jashkasoft/nestjs-json-rpc';
-
-@RpcHandler({
-  method: 'test',
-})
-class Payload {
-  city: string;
-}
-export class TestHandler implements IRpcHandler<Payload> {
-  public async invoke(
-    @RpcPayload() payload: Payload,
-    @RpcVersion() version: string,
-    @RpcId() id: number | string,
-    @RpcMethod() method: string,
-  ) {
-    return payload;
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
